d802e98cadbf19b5bbd35488089de9fa9dd89720
Ryan Keenan <Ryan.Keenan1@marist.edu>
2022-02-10 18:02:01 -0500

    Commit on 'main'

--- JavaLabs/Lab 3/src/base/Main.java
+++ JavaLabs/Lab 3/src/base/Main.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.time.Year;
+
 public class Main {
     /**
      * # Lab Three
@@ -13,11 +15,50 @@ public class Main {
      *     - Else If
      */
     public static void main(String[] args) {
+
         // Make an if statement that triggers a print or println statement
+        // if statement
         // Make an if, else statement where the else statement triggers a print or println statement
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
+        if (1==1) {
+            System.out.println("1==1");
+          }
+          
+          if (1==2) {
+            System.out.println("1==2");
+          } else {
+            System.out.println("1!=2");
+          }
+          
+          if (2==1) {
+            System.out.println("2==1");
+          } else if (2==2) {
+            System.out.println("2==2");
+          } else {
+            System.out.println("2==?");
+          }
+ 
+
         // Make 2 variables and use them in an if else conditional print from one of the sections
+        int one = 1;
+        int two = 2;
+        if (one==one) {
+            System.out.println("1=1");
+        } else {
+            System.out.println("1!=1");
+        }
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
+        if (one==one && two==two) {
+            System.out.println("1=1 and 2=2");
+        } else {
+            System.out.println("1!=1 and 2!=2");
+        }
+
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
+        if (one==two || two==two) {
+            System.out.println("1=2 or 2=2");
+        } else {
+            System.out.println("1!=2 or 2!=2");
+        }
     }
 }
