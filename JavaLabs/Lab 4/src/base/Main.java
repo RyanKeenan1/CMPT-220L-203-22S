5924c4ad3c455578cbd7f5d992a850ceb5bc1100
Ryan Keenan <Ryan.Keenan1@marist.edu>
2022-02-17 17:07:27 -0500

    Money

--- JavaLabs/Lab 4/src/base/Main.java
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.lang.Math;
+
 public class Main {
     /**
      * # Lab Four
@@ -15,11 +17,65 @@ public class Main {
      * - Structuring solutions
      */
     public static void main(String[] args) {
-        // Give me an example of you using switch case.
-        // Give me an example of you using a for loop
-        // Give me an example of you using a while loop
+        // Examples of switch case
+       System.out.println(switchExample("bulldog"));
+       System.out.println(switchExample("golden retriever"));
+       System.out.println(switchExample("tiger"));
+
+        forLoopExample();
+        whileLoopExample();
+
+        System.out.println(raiseToPower(2, 5));
+
+        int[] testArray = {2, 3, 3, 3, 4, 4, 2, 1, 5};
+        System.out.println(numAppearInArray(testArray, 3));
+
+        int[] testArray2 = {2, 3, 3, 3, 4, 4, 2, 1, 5};
+        System.out.println("Sum of all vals in testArray2 is " + summation(testArray2));
+
+        System.out.println("The answer of 3^2 + 4^2 is: " + pythTheroem(3, 4));
     }
     // For the following todos you'll probably want to define them below and then call them from the main function above.
+
+    /**
+     * Returns whether the input is a kanine or not
+     * @param input string that is most likely an animal
+     * @return boolean if input is a type of dog
+     */
+    public static boolean switchExample(String input) {
+        switch(input) {
+            case "bulldog":
+                return true;
+            case "golden retriever":
+                return true;
+            case "rotweiler":
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Example of a for loop which runs 5 times
+     */
+    public static void forLoopExample() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I've run the for loop " + (i + 1) + " times!");
+        }
+    }
+
+    /**
+     * An example of a while loop which runs 5 times
+     */
+    public static void whileLoopExample() {
+        int whileLoopcounter = 0;
+
+        while(whileLoopcounter != 5) {
+            System.out.println("I've run the while loop " + (whileLoopcounter + 1) + " times!");
+            whileLoopcounter++;
+        }
+    }
+
 
     /**
      * I want you to write a function that will take in a number and raise it to the power given.
@@ -27,17 +83,43 @@ public class Main {
      * and should print out or return 8. Print the output.
      */
 
+    public static double raiseToPower(int baseNum, int powerNum) {
+        return Math.pow(baseNum, powerNum);
+    }
+
     /**
      * I want you to write a function that will take in a list and see how many times a given number is in the list.
      * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
      * or return 3. Print the output.
      */
 
+    public static int numAppearInArray(int[] numBoi, int numToFind) {
+        int foundInArray = 0;
+
+        for (int i = 0; i < numBoi.length; i++) {
+            if (numBoi[i] == numToFind) {
+                foundInArray++;
+            }
+        }
+
+        return foundInArray;
+    }
+
     /**
      * Create a function named summation that will take in an int[ ] and return an int.
      * I want the function to then go through the given array and sum up digits in the array.
      * Once that’s done print or return the answer. Print the output
      */
+
+    public static int summation(int[] givenArray) {
+        int sumOfVals = 0;
+
+        for (int i = 0; i < givenArray.length; i++) {
+            sumOfVals += givenArray[i];
+        }
+
+        return sumOfVals;
+    }
 
     /**
      * Give me a function that gives the answer to the pythagorean theorem.
@@ -59,5 +141,9 @@ public class Main {
     int division(int a, int b) {
         int answer = a / b;
         return answer;
+    }
+
+    public static int pythTheroem(int a, int b) {
+        return (int)raiseToPower(a, 2) + (int)raiseToPower(b, 2);
     }
 }
